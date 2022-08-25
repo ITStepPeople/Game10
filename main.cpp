9a8eeafc4f310b40b12f5ef674e4bfa65e3d4936
PieceOfEon <danilskripka1996@gmail.com>
2022-08-25 16:02:56 +0300

    test3

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace sf;
 int main()
 {
 	RenderWindow window(VideoMode(1200,800), "SFML Works!"/*,Style::Fullscreen*/);
-
+	//ddd
 	Sprite spriteFon;
 	Texture textureFon;
 	textureFon.loadFromFile("resources\\Fon.jpg");
