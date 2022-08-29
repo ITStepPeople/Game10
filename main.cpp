ed620c214a6cdf6b2f4fe5901d5bdc1c2ee12cbe
PieceOfEon <danilskripka1996@gmail.com>
2022-08-29 21:42:26 +0300

    Add+

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 #include<iostream>
 using namespace sf;
 
+
 int main()
 {
-	RenderWindow window(VideoMode(1200,800), "SFML Works!"/*,Style::Fullscreen*/);
-	//ddd hehsas
+	RenderWindow window(VideoMode(1200, 800), "SFML Works!"/*,Style::Fullscreen*/);
 	Sprite spriteFon;
 	Texture textureFon;
 	textureFon.loadFromFile("resources\\Fon.jpg");
@@ -18,60 +18,102 @@ int main()
 	window.display();
 	Texture texture;
 	Texture textureRev;
-	texture.loadFromFile("resources\\Cat2.png");
-	textureRev.loadFromFile("resources\\Cat2Rev.png");
+	texture.loadFromFile("resources\\Cat.png");
+	textureRev.loadFromFile("resources\\CatRev.png");
 	Sprite sprite;
-	/*Sprite spriteRev;*/
-	
+
+	int x, y, x1, y1;
+	x = 881, y = 999, x1 = 279, y1 = 201;
 	sprite.setTexture(texture);
-	sprite.setTextureRect(IntRect(881, 999, 279, 201));//x,y,x,y
-	sprite.setPosition(50, 25);//x,y
+	sprite.setTextureRect(IntRect(x, y, x1, y1));//x,y,x,y
+	sprite.setPosition(50, 625);//x,y
+	sprite.setScale(0.7, 0.7);
 
 	//spriteRev.setTexture(textureRev);
 	//spriteRev.setTextureRect(IntRect(881, 999, 279, 201));//x,y,x,y
 	//spriteRev.setPosition(50, 25);//x,y
+	float CurrentFrame = 0;//хранит текущий кадр
+
 	Clock clock;
 
-	float CurrentFrame = 0;//хранит текущий кадр
 
 	while (window.isOpen())
 	{
 		float time = clock.getElapsedTime().asMicroseconds();
 		clock.restart();
-		/*time = time / 800;*/
-		std::cout << time<< "\n";
+		time = time / 800;
+		/*std::cout << time<< "\n";*/
 		Event event;
 		while (window.pollEvent(event))
 		{
 			if (event.type == Event::Closed)
-				
+
 				window.close();
 		}
-		
-		if (Keyboard::isKeyPressed(Keyboard::Left))
+
+		if (Keyboard::isKeyPressed(Keyboard::Left) || Keyboard::isKeyPressed(Keyboard::A))
 		{
 			sprite.setTexture(textureRev);
+			int q;
+			q = 199 * int(CurrentFrame);
+			CurrentFrame += 0.009 * time;
+			if (CurrentFrame > 12)CurrentFrame -= 12;
+			x = 347, y = 0, x1 = 408, y1 = 199;
+			sprite.setTextureRect(IntRect(x, y + q, x1, y1/**int(CurrentFrame)*/));
+			std::cout << sprite.getRotation() << "\n";
+			sprite.move(-0.20 * time, 0);
+			spriteFon.move(0.05 * time, 0);
+			/*sprite.setTexture(textureRev);
 			sprite.move(-0.1*time, 0);
-			sprite.setTextureRect(IntRect(347, 1007, 367, 170));
+			sprite.setTextureRect(IntRect(347, 1007, 367, 170));*/
 		}
-		if (Keyboard::isKeyPressed(Keyboard::Right))
+		if (Keyboard::isKeyPressed(Keyboard::Right) || Keyboard::isKeyPressed(Keyboard::D))
 		{
 			sprite.setTexture(texture);
+			int q;
+			q = 199 * int(CurrentFrame);
+			CurrentFrame += 0.009 * time;
+			x = 1183, y = 0, x1 = 436, y1 = 199;
+			if (CurrentFrame > 12)CurrentFrame -= 12;
+			sprite.setTextureRect(IntRect(x, y + q, x1, y1/**int(CurrentFrame)*/));
+			sprite.move(0.20 * time, 0);
+			spriteFon.move(-0.05 * time, 0);
+
+
+			/*sprite.setTextureRect(IntRect(1183, 7, 436, 183));*/
+			/*sprite.setTexture(texture);
 			sprite.move(0.1 * time, 0);
-			sprite.setTextureRect(IntRect(1227, 1020, 367, 170));
+			sprite.setTextureRect(IntRect(1183, 7, 436, 183));*/
 		}
 		if (Keyboard::isKeyPressed(Keyboard::Up))
 		{
-			sprite.move(0,-0.1 * time); //x,y
+			int q;
+			q = 199 * int(CurrentFrame);
+			x = 825, y = 200, x1 = 355, y1 = 199;
+			sprite.setTexture(texture);
+			if (CurrentFrame > 6)CurrentFrame -= 6;
+			sprite.setTextureRect(IntRect(x, y + q, x1, y1/**int(CurrentFrame)*/));
 		}
 		if (Keyboard::isKeyPressed(Keyboard::Down))
 		{
-			sprite.move(0, 0.1 * time);
+			sprite.setTexture(texture);
+			//sprite.move(0, 0.1/* * time*/);
+			sprite.setTextureRect(IntRect(881, 999, 279, 201));
+			sprite.setRotation(-25);
 		}
-
+		if (!Keyboard::isKeyPressed(Keyboard::Down))
+		{
+			sprite.setRotation(0);
+		}
+		if ((!Keyboard::isKeyPressed(Keyboard::Right)) && (!Keyboard::isKeyPressed(Keyboard::Left)))
+		{
+			sprite.setTexture(texture);
+			sprite.setTextureRect(IntRect(881, 999, 279, 201));
+		}
 		window.clear(Color::White);
 		window.draw(spriteFon);
 		window.draw(sprite);
+
 
 		window.display();
 	}
