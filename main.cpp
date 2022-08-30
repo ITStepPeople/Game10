8aa038002913b99f07a656a36eca9d2b5adb4f42
NyiteX <kitheus09@gmail.com>
2022-08-30 13:17:14 +0300

    code123

--- main.cpp
+++ main.cpp
@@ -7,51 +7,115 @@ using namespace sf;
 int main()
 {
 	RenderWindow window(VideoMode(1200, 800), "SFML Works!"/*,Style::Fullscreen*/);
-	Sprite spriteFon;
-	Texture textureFon;
-	textureFon.loadFromFile("resources\\Fon.jpg");
-	spriteFon.setTexture(textureFon);
 
-	spriteFon.setScale(0.3, 0.3);
-
-	window.draw(spriteFon);
-	window.display();
-	Texture texture;
-	Texture textureRev;
-	texture.loadFromFile("resources\\Cat.png");
-	textureRev.loadFromFile("resources\\CatRev.png");
-	Sprite sprite;
-
+	float CurrentFrame = 0;
+	float CurrentFrame2 = 0;
 	int x, y, x1, y1;
 	x = 881, y = 999, x1 = 279, y1 = 201;
+
+	Texture textureFon;
+	Texture texture;
+	Texture textureRev;
+	Sprite spriteFon;
+	Sprite sprite;
+	texture.loadFromFile("resources\\Cat.png");
+	textureRev.loadFromFile("resources\\CatRev.png");
+	textureFon.loadFromFile("resources\\Fon.jpg");
+
+	spriteFon.setTexture(textureFon);
+	spriteFon.setScale(0.3, 0.3);
 	sprite.setTexture(texture);
-	sprite.setTextureRect(IntRect(x, y, x1, y1));//x,y,x,y
+	sprite.setTextureRect(IntRect(x, y, x1, y1));
 	sprite.setPosition(50, 625);//x,y
 	sprite.setScale(0.7, 0.7);
 
-	//spriteRev.setTexture(textureRev);
-	//spriteRev.setTextureRect(IntRect(881, 999, 279, 201));//x,y,x,y
-	//spriteRev.setPosition(50, 25);//x,y
-	float CurrentFrame = 0;//хранит текущий кадр
+	SoundBuffer sound1;
+	sound1.loadFromFile("resources\\04. The Blinded Forest.flac");
+	Sound testsound;
+	testsound.setBuffer(sound1);
+	testsound.play();
+	testsound.setVolume(30);
 
 	Clock clock;
-
-
+	bool sit = true;
+	bool rightM = false;
+	bool leftM = false;
 	while (window.isOpen())
 	{
+		std::cout << "x: " << x << std::endl;
 		float time = clock.getElapsedTime().asMicroseconds();
 		clock.restart();
 		time = time / 800;
-		/*std::cout << time<< "\n";*/
 		Event event;
 		while (window.pollEvent(event))
 		{
 			if (event.type == Event::Closed)
-
+			{
+				exit(0);
 				window.close();
+			}
 		}
-
-		if (Keyboard::isKeyPressed(Keyboard::Left) || Keyboard::isKeyPressed(Keyboard::A))
+		if (!Keyboard::isKeyPressed(Keyboard::Right) && !Keyboard::isKeyPressed(Keyboard::Left) && sit == true)
+		{
+			if (rightM == true)
+			{
+				x = 820, y = 0, x1 = 360, y1 = 199;
+				int q = 199 * int(CurrentFrame);
+				CurrentFrame += 0.007 * time;
+				if (CurrentFrame > 6)
+				{
+					sit = false;
+					CurrentFrame -= 6;
+				}
+				sprite.setTexture(texture);
+				sprite.setTextureRect(IntRect(x, y + q, x1, y1));
+			}
+			if (leftM == true)
+			{
+				x = 760, y = 0, x1 = 370, y1 = 199;
+				int q = 199 * int(CurrentFrame);
+				CurrentFrame += 0.007 * time;
+				if (CurrentFrame > 6)
+				{
+					sit = false;
+					CurrentFrame -= 6;
+				}
+				sprite.setTexture(textureRev);
+				sprite.setTextureRect(IntRect(x, y + q, x1, y1));
+			}
+		}
+		if (Keyboard::isKeyPressed(Keyboard::LShift) && Keyboard::isKeyPressed(Keyboard::Right))
+		{
+			x = 33, y = 1410, x1 = 365, y1 = 199;
+			int q = 199 * int(CurrentFrame2);
+			CurrentFrame2 += 0.015 * time;
+			if (CurrentFrame2 > 5)
+				CurrentFrame2 -= 13;
+			sprite.setTexture(texture);
+			sprite.setTextureRect(IntRect(x, y + q, x1, y1));
+			sprite.move(0.30 * time, 0);
+			spriteFon.move(-0.10 * time, 0);
+			sit = true;
+			rightM = true;
+			leftM = false;
+		}
+		if (Keyboard::isKeyPressed(Keyboard::LShift) && Keyboard::isKeyPressed(Keyboard::Left))
+		{
+			x = 1550, y = 1410, x1 = 365, y1 = 199;
+			int q = 199 * int(CurrentFrame2);
+			CurrentFrame2 += 0.015 * time;
+			if (CurrentFrame2 > 5)
+				CurrentFrame2 -= 13;
+			sprite.setTexture(textureRev);
+			sprite.setTextureRect(IntRect(x, y + q, x1, y1));
+			/*if(sprite.getPosition() < )*/
+			sprite.move(-0.30 * time, 0);
+			spriteFon.move(0.10 * time, 0);
+			sit = true;
+			rightM = false;
+			leftM = true;
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Left) && !Keyboard::isKeyPressed(Keyboard::LShift))
 		{
 			sprite.setTexture(textureRev);
 			int q;
@@ -59,65 +123,31 @@ int main()
 			CurrentFrame += 0.009 * time;
 			if (CurrentFrame > 12)CurrentFrame -= 12;
 			x = 347, y = 0, x1 = 408, y1 = 199;
-			sprite.setTextureRect(IntRect(x, y + q, x1, y1/**int(CurrentFrame)*/));
-			std::cout << sprite.getRotation() << "\n";
+			sprite.setTextureRect(IntRect(x, y + q, x1, y1));
 			sprite.move(-0.20 * time, 0);
 			spriteFon.move(0.05 * time, 0);
-			/*sprite.setTexture(textureRev);
-			sprite.move(-0.1*time, 0);
-			sprite.setTextureRect(IntRect(347, 1007, 367, 170));*/
+			sit = true;
+			leftM = true;
+			rightM = false;
 		}
-		if (Keyboard::isKeyPressed(Keyboard::Right) || Keyboard::isKeyPressed(Keyboard::D))
+		if (Keyboard::isKeyPressed(Keyboard::Right) && !Keyboard::isKeyPressed(Keyboard::LShift))
 		{
 			sprite.setTexture(texture);
-			int q;
-			q = 199 * int(CurrentFrame);
+			int q = 199 * int(CurrentFrame);
 			CurrentFrame += 0.009 * time;
 			x = 1183, y = 0, x1 = 436, y1 = 199;
 			if (CurrentFrame > 12)CurrentFrame -= 12;
-			sprite.setTextureRect(IntRect(x, y + q, x1, y1/**int(CurrentFrame)*/));
+			sprite.setTextureRect(IntRect(x, y + q, x1, y1));
 			sprite.move(0.20 * time, 0);
 			spriteFon.move(-0.05 * time, 0);
-
-
-			/*sprite.setTextureRect(IntRect(1183, 7, 436, 183));*/
-			/*sprite.setTexture(texture);
-			sprite.move(0.1 * time, 0);
-			sprite.setTextureRect(IntRect(1183, 7, 436, 183));*/
-		}
-		//123test
-		if (Keyboard::isKeyPressed(Keyboard::Up))
-		{
-			int q;
-			q = 199 * int(CurrentFrame);
-			x = 825, y = 200, x1 = 355, y1 = 199;
-			sprite.setTexture(texture);
-			if (CurrentFrame > 6)CurrentFrame -= 6;
-			sprite.setTextureRect(IntRect(x, y + q, x1, y1/**int(CurrentFrame)*/));
-		}
-		if (Keyboard::isKeyPressed(Keyboard::Down))
-		{
-			sprite.setTexture(texture);
-			//sprite.move(0, 0.1/* * time*/);
-			sprite.setTextureRect(IntRect(881, 999, 279, 201));
-			sprite.setRotation(-25);
-		}
-		if (!Keyboard::isKeyPressed(Keyboard::Down))
-		{
-			sprite.setRotation(0);
-		}
-		if ((!Keyboard::isKeyPressed(Keyboard::Right)) && (!Keyboard::isKeyPressed(Keyboard::Left)))
-		{
-			sprite.setTexture(texture);
-			sprite.setTextureRect(IntRect(881, 999, 279, 201));
+			sit = true;
+			rightM = true;
+			leftM = false;
 		}
 		window.clear(Color::White);
 		window.draw(spriteFon);
 		window.draw(sprite);
-
-
 		window.display();
 	}
-
 	return 0;
 }
